4b61d16871f9fa9250e99fc4dc88cdf4e0c8b927
Jaxzynn <jaxzynnice@gmail.com>
2025-06-01 11:55:55 +0700

    Save quotes.js.

--- src/api/random/quotes.js
+++ src/api/random/quotes.js
@@ -2,29 +2,19 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 
 module.exports = function(app) {
-    async function quote() {
+    async function hadits() {
         try {
-            const response = await axios.get("https://quotes.toscrape.com/random", {
-                headers: {
-                    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
-                },
-            });
-
-            const $ = cheerio.load(response.data);
-            const quote = $(".quote .text").text().trim();
-            const author = $(".quote .author").text().trim();
-            const tags = $(".quote .tags .tag")
-            .map((_, el) => $(el).text().trim())
-            .get();
-
-            if (!quote || !author) {
-                throw new Error("Invalid response structure");
-            }
+            const {
+                data
+            } = await axios.get('https://api.myquran.com/v2/hadits/arbain/semua');
+            const datanya = data.data;
+            const randomData = datanya[Math.floor(Math.random() * datanya.length)];
 
             return {
-                quote,
-                author,
-                tags
+                index: randomData.no,
+                title: randomData.judul,
+                arabic: randomData.arab,
+                indonesian: randomData.indo
             };
         } catch (error) {
             console.error(error);
@@ -32,10 +22,9 @@ module.exports = function(app) {
         }
     }
 
-
-    app.get('/random/quotes', async (req, res) => {
+    app.get('/random/hadits', async (req, res) => {
         try {
-            const msg = await quote(text);
+            const msg = await hadist(text);
             res.status(200).json({
                 status: true,
                 data: msg
